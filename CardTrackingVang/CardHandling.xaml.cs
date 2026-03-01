dc66620c8927c838b879451527ba922f87d4f14a
Evan Vang <evankvang@gmail.com>
2026-03-01 09:05:44 -0600

    Set a mobile margning

--- CardTrackingVang/CardHandling.xaml.cs
+++ CardTrackingVang/CardHandling.xaml.cs
@@ -6,4 +6,10 @@ public partial class CardHandling : ContentPage
 	{
 		InitializeComponent();
 	}
+
+    private async void AddCardBtn_Clicked(object sender, EventArgs e)
+    {
+		// Navigate to add card page.
+		await DisplayAlertAsync("Add card", "Implementing add page soon", "OK");
+    }
 }
