233e0f7952a7fe53074583ec82aa519fe55e3be4
Evan Vang <evankvang@gmail.com>
2026-03-03 00:24:32 -0600

    Shared component of my header

---
+++ CardTrackingVang/HeaderApp.xaml.cs
@@ -0,0 +1,11 @@
+namespace CardTrackingVang.Header;
+
+// https://stackoverflow.com/questions/74751527/how-to-create-reusable-components-in-net-maui
+public partial class HeaderApp : ContentView
+{
+	public HeaderApp()
+	{
+		// NOTE TO SELF MUST BE A CONTENT VIEW TO MAKE IT SHARED
+		InitializeComponent();
+	}
+}
