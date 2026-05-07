96fec23f7b586fa20407185dc85a097ac4e99762
Evan Vang <evankvang@gmail.com>
2026-05-06 23:40:44 -0500

    Custom labels.

---
+++ CardTrackingVang/CustomControls/HelpInfoLabel.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardTrackingVang.CustomControls
+{
+    public class HelpInfoLabel : Label
+    {
+        public HelpInfoLabel()
+        {
+            TextColor = Colors.Gray;
+        }
+    }
+}
---
+++ CardTrackingVang/CustomControls/HelpLabel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace CardTrackingVang.CustomControls
+{
+    public class HelpLabel : Label
+    {
+        public HelpLabel()
+        {
+            FontSize = 16;
+            FontAttributes = FontAttributes.Bold;
+            TextColor = Colors.Black;
+        }
+    }
+}
